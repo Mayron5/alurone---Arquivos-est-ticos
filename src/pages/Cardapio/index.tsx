54d52b8730c03624dc3db9d9da50969dfbd8687b
Mayron Alves <mayronalves@alu.ufc.br>
2022-07-24 01:30:11 -0300

    Projeto base!

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Cardapio.module.scss';
+
+const Cardapio = () => {
+  return (
+    <main>
+      <nav className={styles.menu}>
+
+      </nav>
+    </main>
+  );
+};
+
+export default Cardapio;
