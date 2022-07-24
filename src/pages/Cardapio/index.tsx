07f0e94fe8d57db9c80fb999f4a9225e325b0ede
Mayron Alves <mayronalves@alu.ufc.br>
2022-07-24 01:53:43 -0300

    Configuração de arquivos estáticos.

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,12 @@
 import styles from './Cardapio.module.scss';
 
+import { ReactComponent as Logo } from 'assets/logo.svg';
+
 const Cardapio = () => {
   return (
     <main>
       <nav className={styles.menu}>
-
+        <Logo />
       </nav>
     </main>
   );
